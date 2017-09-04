b7ffc329d2115789403dd022ae61f813974b72c4
elbincodes <elbincodes@gmail.com>
2017-09-04 17:46:08 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender) VALUES ("MR. Chocolate", 8, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Rowdy", 9, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Tabitha", 9, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Sergeant Brown", 8, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Melissa", 7, "F");
+INSERT INTO bears (name, age, gender) VALUES ("Grinch", 10, "M");
+INSERT INTO bears (name, age, gender) VALUES ("Wendy", 9, "F");
+INSERT INTO bears (name, age, gender) VALUES (NULL, 8, "F");
